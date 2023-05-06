e260b54be042b749efd0cc2ac0f9695fc6c8e3bd
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-05-05 19:08:00 -0600

    compiled files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Pay from './components/Pay';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV)
+function App() {
+  return (
+    <div className="App">
+<Pay/>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pay.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import Logo from "../images/logo.JPG";
+import "../styles/Pay.css";
+
+const Key =
+  "pk_test_51N3odWEAjO8uMs3f1qBeYu9iZ4g7FFRssNBrsPM8nzEqmqhrmbLA2UkFrRZyumL57BpT1qJliAc5upYExb2t9C8n00VXARfW73";
+
+const Pay = () => {
+  const [stripetoken, setstripeToken] = useState(null);
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
+
+  const ontoken = (token) => {
+    setstripeToken(token);
+  };
+
+  useEffect(() => {
+    const makerequest = async () => {
+      try {
+        const res = await axios.post("http://localhost:5000/api/payment", {
+          tokenId: stripetoken,
+          amount: 2000,
+        });
+        if (res.data.success) {
+          setPaymentSuccess(true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (stripetoken) {
+      makerequest();
+    }
+  }, [stripetoken]);
+
+  return (
+    <div className="Pay">
+      {paymentSuccess ? (
+        <div className="success-prompt">
+          <p>Payment successful!</p>
+        </div>
+      ) : (
+        <StripeCheckout
+          name="the kill"
+          image={Logo}
+          billingAddress
+          shippingAddress
+          description="your total is $50"
+          amount={5000}
+          token={ontoken}
+          stripeKey={Key}
+        >
+          <button className="payment">Payment</button>
+        </StripeCheckout>
+      )}
+    </div>
+  );
+};
+
+export default Pay;
